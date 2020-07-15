84f35a2d3c879e18af79c5c9d5ee957116e902a5
kadirermaya <kadir.ermaya@gmail.com>
2020-07-14 23:17:29 -0700

    Delete method created.

--- SuperHero/Models/SuperHero.cs
+++ SuperHero/Models/SuperHero.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SuperHero.Models
 {
-    public class SuperHero
+  
+    public class Hero
     {
         [Key]
         public int Id { get; set; }
